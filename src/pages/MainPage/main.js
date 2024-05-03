950dc855b6d5a1ddc26cf71e724e2c728a8ae2d9
Ria <rlathdus788@naver.com>
2024-05-03 11:00:57 +0900

    all changes

---
+++ src/pages/MainPage/main.js
@@ -0,0 +1,13 @@
+// Main.js
+import React from 'react';
+
+function Main() {
+    return (
+        <div className="main-content">
+            여기는 메인 컨텐츠 입니다.
+        </div>
+    );
+}
+
+export default Main;
+
