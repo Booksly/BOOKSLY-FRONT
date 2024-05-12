f8f95fe267f3aaf24d3d002f17791e2959152bc0
juytj21 <juytj21@kyonggi.ac.kr>
2024-05-12 16:05:59 +0900

    라우팅

--- src/Routes/Routing.js
+++ src/Routes/Routing.js
@@ -1,49 +1,41 @@
 import React from "react";
-import App from "../App";
+import App from '../App';
+
+// My Home 페이지
 import MyHomeMain from "../pages/MyHomeMainPage/MyHomeMain";
-import StoreSearch from "../pages/StoreSearchPage/StoreSearchPage";
-import DetailedStoreInfo from "../pages/DetailedStoreInfoPage/DetailedStoreInfo";
-import Reservation from "../pages/ReservationPage/Reservation";
-import TodayReservation from "../pages/TodayReservationPage/TodayReservationPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MyHomeAllReserv from "../pages/MyHomeAllReservPage/MyHomeAllReserv";
 import MyHomeCurrReserv from "../pages/MyHomeCurrReservPage/MyHomeCurrReserv";
 import MyHomeRegularShop from "../pages/MyHomeRegularShopPage/MyHomeRegularShop";
-import StoreSearch from "../pages/StoreSearchPage/StoreSearchPage";
-import DetailedStoreInfo from "../pages/DetailedStoreInfoPage/DetailedStoreInfo";
-import Reservation from "../pages/ReservationPage/Reservation";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// 가게 페이지
+import StoreSearch from '../pages/StoreSearchPage/StoreSearchPage';
+import DetailedStoreInfo from '../pages/DetailedStoreInfoPage/DetailedStoreInfo';
+import Reservation from '../pages/ReservationPage/Reservation';
+
+// 가게 주인 페이지
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 function Routing() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/MyHomeMain" element={<MyHomeMain />} />
-          <Route path="/StoreSearch" element={<StoreSearch />} />
-          <Route path="/DetailedStoreInfo" element={<DetailedStoreInfo />} />
-          <Route path="/Reservation" element={<Reservation />} />
-          <Route path="/TodayReservation" element={<TodayReservation />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/MyHomeMain" element={<MyHomeMain />} />
-          <Route path="/MyHomeAllReserv" element={<MyHomeAllReserv />} />
-          <Route path="/MyHomeCurrReserv" element={<MyHomeCurrReserv />} />
-          <Route path="/MyHomeRegularShop" element={<MyHomeRegularShop />} />
-          <Route path="/StoreSearch" element={<StoreSearch />} />
-          <Route path="/DetailedStoreInfo" element={<DetailedStoreInfo />} />
-          <Route path="/Reservation" element={<Reservation />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<App/>}/>
+                    {/*고객 My Home*/}
+                    <Route path='/MyHomeMain' element={<MyHomeMain/>}/>
+                    <Route path='/MyHomeAllReserv' element={<MyHomeAllReserv/>}/>
+                    <Route path='/MyHomeCurrReserv' element={<MyHomeCurrReserv/>}/>
+                    <Route path='/MyHomeRegularShop' element={<MyHomeRegularShop/>}/>
+                    {/*고객 가게 관련 페이지*/}
+                    <Route path='/StoreSearch' element={<StoreSearch/>}/>
+                    <Route path='/DetailedStoreInfo' element={<DetailedStoreInfo/>}/>
+                    <Route path='/Reservation' element={<Reservation/>}/>
+                    {/*가게주인용*/}
+                    <Route></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
 
-export default Routing;
+export default Routing
