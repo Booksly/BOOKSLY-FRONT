8177cbdcc6e14216b9b8675cbc8090dd1ed84bd3
juytj21 <juytj21@kyonggi.ac.kr>
2024-05-17 10:59:26 +0900

    고객로그인페이지

--- src/pages/LogInPage/CustomerLogInPage.js
+++ src/pages/LogInPage/CustomerLogInPage.js
@@ -5,34 +5,16 @@ import line from "../../assets/Line_img.png";
 import naver from "../../assets/Naver_icon.png";
 import google from "../../assets/Google_icon.png";
 import kakao from "../../assets/Kakao_icon.png";
+import LoginPageNav from "../nav/LoginPageNav";
 
 export default function CustomerLoginPage() {
   return (
     <div className="CustomerLoginPage">
       <div className="CustomerLoginPageBack">
+        <LoginPageNav/>
         <div className="container-1">
-          <div className="login-header">
-            <div className="header-inner-left">
-              <div className="logo-goto-home">
-                <div className="image-13"></div>
-              </div>
-              <div className="header-buttons">
-                <div className="nav-box">
-                  <span className="container-5">가게조회</span>
-                </div>
-              </div>
-            </div>
-            <div className="header-inner-right">
-              <div className="icon-home">
-                <img className="vector" src={home} alt={""} />
-              </div>
-              <div className="go-mypage">
-                <span className="container">북슬리 홈</span>
-              </div>
-            </div>
-          </div>
-          <div className="container">
-            <div className="container-1">
+          <div className="CustomerLoginPageContainer">
+            <div className="loginBox">
               <div className="line-5"></div>
               <div className="frame-10">
                 <span className="container-2">개인회원</span>
--- src/pages/nav/LoginPageNav.js
+++ src/pages/nav/LoginPageNav.js
@@ -1,34 +1,33 @@
 import './LoginPageNav.css'
-import searchbar from "../../assets/search_button.png";
 import home from "../../assets/home_button.png";
 
 export default function LoginPageNav() {
     return (
         <div className="LoginPageNav">
             <div className="LoginPageNavBack">
-                <div className="header-inner-before">
-                    <div className="header-inner-left">
-                        <div className="logo-goto-home">
-                            <div className="booksly-logo">
+                <div className="LoginPageNavheader-inner-before">
+                    <div className="LoginPageNavheader-inner-left">
+                        <div className="LoginPageNavlogo-goto-home">
+                            <div className="LoginPageNavbooksly-logo">
                             </div>
                         </div>
-                        <div className="header-buttons">
-                            <div className="currentNav">
-                                <div className="currentPageBar">
-                                    <span className="PageName">
+                        <div className="LoginPageNavheader-buttons">
+                            <div className="LoginPageNavcurrentNav">
+                                <div className="LoginPageNavcurrentPageBar">
+                                    <span className="LoginPageNavPageName">
                                         가게조회
                                     </span>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="header-inner-right">
-                        <div className="headerRightDiv">
-                            <div className="icon">
-                                <img className="home" src={home} alt={''}/>
+                    <div className="LoginPageNavheader-inner-right">
+                        <div className="LoginPageNavheaderRightDiv">
+                            <div className="LoginPageNavicon">
+                                <img className="LoginPageNavhome" src={home} alt={''}/>
                             </div>
-                            <div className="go-mypage">
-                                <span className="my-page">
+                            <div className="LoginPageNavgo-mypage">
+                                <span className="LoginPageNavmy-page">
                                     북슬리 홈
                                 </span>
                             </div>
