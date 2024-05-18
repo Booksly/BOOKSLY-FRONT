31d4d6bb0575e6dbcb314180b3c13268c9e87932
Ria <rlathdus788@naver.com>
2024-05-18 21:26:28 +0900

    개인회원 로그인 페이지 수정 및 개발 완료

--- src/pages/LogInPage/CustomerLogInPage.js
+++ src/pages/LogInPage/CustomerLogInPage.js
@@ -11,40 +11,32 @@ export default function CustomerLoginPage() {
     <div className="CustomerLoginPage">
       <div className="CustomerLoginPageBack">
         <LoginPageNav />
-        <div className="container-1">
-          <div className="CustomerLoginPageContainer">
-            <div className="loginBox">
-              <div className="line-5"></div>
-              <div className="frame-10">
-                <span className="container-2">개인회원</span>
-                <span className="container-3">기업회원</span>
+        <div className="CustomerLoginPageContainer">
+          <div className="loginBox">
+            <div className="line-5"></div>
+            <div className="frame-10">
+              <span className="container-2">개인회원</span>
+              <span className="container-3">기업회원</span>
+            </div>
+            <div className="container-2">
+              <div className="line-6"></div>
+              <span className="container-4">소셜 계정으로 간편 로그인</span>
+              <img className="line-7" src={line} alt={""} />
+            </div>
+            <div className="Logo_Container">
+              <div className="Naver_Frame">
+                <img className="N_Logo" src={naver} alt={""} />
               </div>
-              <div className="container-2">
-                <div className="line-6"></div>
-                <span className="container-4">소셜 계정으로 간편 로그인</span>
-                <img className="line-7" src={line} alt={""} />
+              <div className="Kakao_Frame">
+                <img className="Kakao_Logo" src={kakao} alt={""} />
               </div>
-              <div className="container-3">
-                <div className="frame-12">
-                  <img className="vector-1" src={naver} alt={""} />
-                </div>
-                <div className="frame-13">
-                  <div className="rikakao-talk-fill">
-                    <img className="vector-2" src={kakao} alt={""} />
-                  </div>
-                </div>
-                <div className="frame-14">
-                  <img
-                    className="flat-color-iconsgoogle"
-                    src={google}
-                    alt={""}
-                  />
-                </div>
+              <div className="frame-14">
+                <img className="Google_Logo" src={google} alt={""} />
               </div>
             </div>
-            <img className="rectangle-7" src={advert} alt={""} />
           </div>
         </div>
+        <img className="rectangle-7" src={advert} alt={""} />
       </div>
     </div>
   );
