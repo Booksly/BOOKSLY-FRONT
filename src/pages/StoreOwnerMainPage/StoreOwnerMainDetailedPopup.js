f6c44b5b33279afe3d97b882e5ffc287aa9cee81
juytj21 <juytj21@kyonggi.ac.kr>
2024-05-24 09:58:01 +0900

    팝업

---
+++ src/pages/StoreOwnerMainPage/StoreOwnerMainDetailedPopup.js
@@ -0,0 +1,338 @@
+import React from "react";
+import {useNavigate} from "react-router-dom";
+import './StoreOwnerMainDetailedPopup.css'
+
+export default function StoreOwnerMainDetailedPopup() {
+    const navigate = useNavigate()
+    return (
+        <div className="StoreOwnerMainDetailedPopup">
+            <div className="DetailedPopupstore-detailed-popup">
+                <div className="DetailedPopupstore-detailed-popup-left">
+                    <div className="DetailedPopupstore-name-box">
+                        <div className="DetailedPopupstore-name">
+                            <div className="DetailedPopupcontainer-52">
+                                상호
+                            </div>
+                        </div>
+                        <div className="DetailedPopupstore-name-space">
+                            <div className="DetailedPopupcontainer-53">
+                                제이제이 헤어
+                            </div>
+                        </div>
+                    </div>
+                    <div className="DetailedPopupstore-main-box">
+                        <div className="DetailedPopupstore-main">
+                            <div className="DetailedPopupcontainer">
+                                메인
+                            </div>
+                        </div>
+                        <div className="DetailedPopupstore-main-space">
+                            <div className="DetailedPopupcontainer-48">
+                                고객 맞춤 프리미엄 서비스를 제공하는 제이제이 헤어입니다.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="DetailedPopuplocation-box">
+                        <div className="DetailedPopuplocation">
+                            <div className="DetailedPopuplocation-icon">
+                            </div>
+                        </div>
+                        <div className="DetailedPopuplocation-address-box">
+                            <div className="DetailedPopuplocation-search-box">
+                                <div className="DetailedPopuplocation-search">
+                                    <div className="DetailedPopupcontainer-42">
+                                        도로명, 지번, 건물명 검색
+                                    </div>
+                                </div>
+                                <div className="DetailedPopuplocation-search-button">
+                                <div className="DetailedPopupcontainer-40">
+                                    검색
+                                </div>
+                                </div>
+                            </div>
+                            <div className="DetailedPopuplocation-address">
+                                <div className="DetailedPopupcontainer-46">
+                                    경기도 수원시 영통구 광교산로 154-42
+                                </div>
+                            </div>
+                            <div className="DetailedPopuplocation-detailed-address">
+                                <div className="DetailedPopupcontainer-47">
+                                    206호
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="DetailedPopuplocation-plus-box">
+                        <div className="DetailedPopuplocation-plus-icon">
+                            <div className="DetailedPopupcontainer-45">
+                            </div>
+                        </div>
+                        <div className="DetailedPopuplocation-plus">
+                            <div className="DetailedPopupcontainer-49">
+                                위치 추가 설명
+                            </div>
+                        </div>
+                    </div>
+                    <div className="DetailedPopupphones-box">
+                        <div className="DetailedPopupphone-icon">
+                            <div className="DetailedPopupcontainer-44">
+                            </div>
+                        </div>
+                        <div className="DetailedPopupphones">
+                            <div className="DetailedPopupphone-box">
+                                <div className="DetailedPopupcontainer-1">
+                                    전화번호
+                                </div>
+                                <div className="DetailedPopupphone-space">
+                                    <div className="DetailedPopupcontainer-51">
+                                        031-249-9114
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="DetailedPopupinsta-box">
+                                <div className="DetailedPopupcontainer-2">
+                                    인스타그램
+                                </div>
+                                <div className="DetailedPopupinsta-space">
+                                    <div className="DetailedPopuphttpinstagram">
+                                        http://instagram. ...
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="DetailedPopupkakao-box">
+                                <div className="DetailedPopupcontainer-3">
+                                    카카오톡
+                                </div>
+                                <div className="DetailedPopupkakao-space">
+                                    <div className="DetailedPopuphttppf-kakao-com">
+                                        http://pf.kakao.com/...
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="DetailedPopupblog-box">
+                                <div className="DetailedPopupcontainer-50">
+                                    블로그
+                                </div>
+                                <div className="DetailedPopupblog-space">
+                                    <div className="DetailedPopuph">
+                                        h
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="DetailedPopupstore-detailed-popup-right">
+                    <div className="DetailedPopupstore-detailed-info-pic">
+                        <div className="DetailedPopupprofile-select">
+                        </div>
+                    </div>
+                    <div className="DetailedPopuptime-box">
+                        <div className="DetailedPopuptime-icon">
+                            <div className="DetailedPopupcontainer-43">
+                            </div>
+                        </div>
+                        <div className="DetailedPopuptime-space">
+                            <div className="DetailedPopupframe-254">
+                                <div className="DetailedPopupframe-251">
+                                    <div className="DetailedPopupcontainer-4">
+                                        월
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-253">
+                                    <div className="DetailedPopupframe-249">
+                                        <div className="DetailedPopupcontainer-5">
+                                            10:00
+                                        </div>
+                                    </div>
+                                    <div className="DetailedPopupcontainer-6">
+                                        ~
+                                    </div>
+                                    <div className="DetailedPopupframe-250">
+                                        <div className="DetailedPopupcontainer-7">
+                                            20:00
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-319">
+                                    <div className="DetailedPopupcontainer-8">
+                                        휴무
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="DetailedPopupframe-551">
+                                <div className="DetailedPopupframe-2511">
+                                    <div className="DetailedPopupcontainer-9">
+                                        화
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-2531">
+                                    <div className="DetailedPopupframe-2491">
+                                        <div className="DetailedPopupcontainer-10">
+                                            10:00
+                                        </div>
+                                    </div>
+                                    <div className="DetailedPopupcontainer-11">
+                                        ~
+                                    </div>
+                                    <div className="DetailedPopupframe-2501">
+                                        <div className="DetailedPopupcontainer-12">
+                                            20:00
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-3191">
+                                    <div className="DetailedPopupcontainer-13">
+                                        휴무
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="DetailedPopupframe-552">
+                                <div className="DetailedPopupframe-2512">
+                                    <div className="DetailedPopupcontainer-14">
+                                        수
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-2532">
+                                    <div className="DetailedPopupframe-2492">
+                                        <div className="DetailedPopupcontainer-15">
+                                            10:00
+                                        </div>
+                                    </div>
+                                    <div className="DetailedPopupcontainer-16">
+                                        ~
+                                    </div>
+                                    <div className="DetailedPopupframe-2502">
+                                        <div className="DetailedPopupcontainer-17">
+                                            20:00
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-3192">
+                                    <div className="DetailedPopupcontainer-18">
+                                        휴무
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="DetailedPopupframe-553">
+                                <div className="DetailedPopupframe-2513">
+                                    <div className="DetailedPopupcontainer-19">
+                                        목
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-2533">
+                                    <div className="DetailedPopupframe-2493">
+                                        <div className="DetailedPopupcontainer-20">
+                                            10:00
+                                        </div>
+                                    </div>
+                                    <div className="DetailedPopupcontainer-21">
+                                        ~
+                                    </div>
+                                    <div className="DetailedPopupframe-2503">
+                                        <div className="DetailedPopupcontainer-22">
+                                            20:00
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-3193">
+                                    <div className="DetailedPopupcontainer-23">
+                                        휴무
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="DetailedPopupframe-554">
+                                <div className="DetailedPopupframe-2514">
+                                    <div className="DetailedPopupcontainer-24">
+                                        금
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-2534">
+                                    <div className="DetailedPopupframe-2494">
+                                        <div className="DetailedPopupcontainer-25">
+                                            10:00
+                                        </div>
+                                    </div>
+                                    <div className="DetailedPopupcontainer-26">
+                                        ~
+                                    </div>
+                                    <div className="DetailedPopupframe-2504">
+                                        <div className="DetailedPopupcontainer-27">
+                                            20:00
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-3194">
+                                    <div className="DetailedPopupcontainer-28">
+                                        휴무
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="DetailedPopupframe-5521">
+                                <div className="DetailedPopupframe-2515">
+                                    <div className="DetailedPopupcontainer-29">
+                                        토
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-2535">
+                                    <div className="DetailedPopupframe-2495">
+                                        <div className="DetailedPopupcontainer-30">
+                                            10:00
+                                        </div>
+                                    </div>
+                                    <div className="DetailedPopupcontainer-31">
+                                        ~
+                                    </div>
+                                    <div className="DetailedPopupframe-2505">
+                                        <div className="DetailedPopupcontainer-32">
+                                            20:00
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-3195">
+                                    <div className="DetailedPopupcontainer-33">
+                                        휴무
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="DetailedPopupframe-555">
+                                <div className="DetailedPopupframe-2516">
+                                    <div className="DetailedPopupcontainer-34">
+                                        일
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-2536">
+                                    <div className="DetailedPopupframe-2496">
+                                        <div className="DetailedPopupcontainer-35">
+                                            10:00
+                                        </div>
+                                    </div>
+                                    <div className="DetailedPopupcontainer-36">
+                                        ~
+                                    </div>
+                                    <div className="DetailedPopupframe-2506">
+                                        <div className="DetailedPopupcontainer-37">
+                                            20:00
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="DetailedPopupframe-3196">
+                                    <div className="DetailedPopupcontainer-38">
+                                        휴무
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="DetailedPopupframe-575">
+                        <div className="DetailedPopupmodify-button">
+                    <div className="DetailedPopupcontainer-41">
+                        수정
+                    </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
