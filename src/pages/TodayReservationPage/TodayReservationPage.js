5c859479d2f8d83ffd82465b1308ec935628ff37
jiwon0226 <ppg0226@naver.com>
2024-06-05 06:02:02 +0900

    데이터 소량 수정

--- src/pages/TodayReservationPage/TodayReservationPage.js
+++ src/pages/TodayReservationPage/TodayReservationPage.js
@@ -144,18 +144,18 @@ const ResearchResults2 = ({ stores }) => (
     { name: "제이제이 헤어", image: Image1, rating: 4.4, category: "헤어" },
     { name: "네일 샵", image: Image2, rating: 4.8, category: "네일" },
     { name: "마사지 샵", image: Image3, rating: 4.7, category: "마사지" },
-    { name: "피부 관리", image: Image4, rating: 4.9, category: "피부" },
+    { name: "피부 관리", image: Image4, rating: 4.9, category: "왁싱/제모" },
     { name: "속눈썹 샵", image: Image5, rating: 4.3, category: "속눈썹" },
     { name: "뷰티 샵", image: Image6, rating: 4.5, category: "뷰티" },
     { name: "헤어 샵", image: Image7, rating: 4.6, category: "헤어" },
     { name: "네일 아트", image: Image8, rating: 4.8, category: "네일" },
     { name: "마사지 샵", image: Image9, rating: 4.7, category: "마사지" },
-    { name: "피부 관리", image: Image10, rating: 4.9, category: "피부" },
+    { name: "피부 관리", image: Image10, rating: 4.9, category: "왁싱/제모" },
     { name: "뷰티 앤 웰니스", image: Image16, rating: 4.5, category: "기타" },
     { name: "헤어 디자인", image: Image17, rating: 4.6, category: "헤어" },
     { name: "네일 앤 팁", image: Image18, rating: 4.8, category: "네일" },
     { name: "릴렉싱 마사지", image: Image19, rating: 4.7, category: "마사지" },
-    { name: "스파 트리트먼트", image: Image20, rating: 4.9, category: "피부" },
+    { name: "스파 트리트먼트", image: Image20, rating: 4.9, category: "왁싱/제모" },
   ];
 
   //타임 세일 가게
@@ -163,18 +163,18 @@ const ResearchResults2 = ({ stores }) => (
     { name: "프레쉬 헤어", image: Image11, rating: 4.4, category: "헤어" },
     { name: "네일 스튜디오", image: Image12, rating: 4.8, category: "네일" },
     { name: "아로마 마사지", image: Image13, rating: 4.7, category: "마사지" },
-    { name: "스킨케어 솔루션", image: Image14, rating: 4.9, category: "피부" },
+    { name: "스킨케어 솔루션", image: Image14, rating: 4.9, category: "왁싱/제모" },
     { name: "플래시 래쉬", image: Image15, rating: 4.3, category: "속눈썹" },
     { name: "뷰티 앤 웰니스", image: Image16, rating: 4.5, category: "기타" },
     { name: "헤어 디자인", image: Image17, rating: 4.6, category: "헤어" },
     { name: "네일 앤 팁", image: Image18, rating: 4.8, category: "네일" },
     { name: "릴렉싱 마사지", image: Image19, rating: 4.7, category: "마사지" },
-    { name: "스파 트리트먼트", image: Image20, rating: 4.9, category: "피부" },
+    { name: "스파 트리트먼트", image: Image20, rating: 4.9, category: "왁싱/제모" },
     { name: "뷰티 샵", image: Image6, rating: 4.5, category: "뷰티" },
     { name: "헤어 샵", image: Image7, rating: 4.6, category: "헤어" },
     { name: "네일 아트", image: Image8, rating: 4.8, category: "네일" },
     { name: "마사지 샵", image: Image9, rating: 4.7, category: "마사지" },
-    { name: "피부 관리", image: Image10, rating: 4.9, category: "피부" },
+    { name: "피부 관리", image: Image10, rating: 4.9, category: "왁싱/제모" },
     ];
 
 //탑백 스토어
