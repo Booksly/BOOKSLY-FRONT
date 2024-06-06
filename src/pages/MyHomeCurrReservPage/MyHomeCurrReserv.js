f166fc2344380ce9b02dbc313cafbbb327c69187
Ria <rlathdus788@naver.com>
2024-06-06 20:34:57 +0900

    현재 예약 조회 더미 데이터 수정

--- src/pages/MyHomeCurrReservPage/MyHomeCurrReserv.js
+++ src/pages/MyHomeCurrReservPage/MyHomeCurrReserv.js
@@ -1,180 +1,126 @@
-import './MyHomeCurrReserv.css'
+import "./MyHomeCurrReserv.css";
 import MyHomeLeftNavi from "../nav/MyHomeLeftNavi";
 import MyHomeTopNavi from "../nav/MyHomeTopNavi";
 import Select from "react-select";
-import React, {useEffect, useState, useRef} from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 let options = [
-    {value: "all", label: "전체"},
-    {value: "hair", label: "헤어"},
-    {value: "nail", label: "네일"},
-    {value: "eye", label: "속눈썹 / 눈썹"},
-    {value: "massage", label: "마사지"},
-    {value: "makeup", label: "메이크업"},
-    {value: "etc", label: "기타"}
-]
+  { value: "all", label: "전체" },
+  { value: "hair", label: "헤어" },
+  { value: "nail", label: "네일" },
+  { value: "eye", label: "속눈썹 / 눈썹" },
+  { value: "massage", label: "마사지" },
+  { value: "makeup", label: "메이크업" },
+  { value: "etc", label: "기타" },
+];
 
 export default function MyHomeCurrReserv() {
-    const [selectValue, setSelectValue] = useState('')
-    const selectInputRef = useRef(null);
+  const [selectValue, setSelectValue] = useState("");
+  const selectInputRef = useRef(null);
 
-    return (
-        <div className="MyHomeCurrReserv">
-            <div className="MyHomeCurrReservBack">
-                <MyHomeLeftNavi/>
-                <div className="containerAll">
-                    <MyHomeTopNavi/>
-                    <div className="MyHomeCurrReservContainer">
-                        <div className="Content">
-                            <div className="sortingBox">
-                                <>
-                                    <Select
-                                        className="sortBox"
-                                        ref = {selectInputRef}
-                                        onChange={(e) => {
-                                            if (e) {
-                                                setSelectValue(e.value);
-                                            } else {
-                                                setSelectValue("");
-                                            }
-                                        }}
-                                        options={options}
-                                        placeholder="전체"
-                                    />
-                                </>
-                            </div>
-                            <div className="reservList">
-                                <div className="currentReservBox">
-                                    <div className="storeInfoFrame">
-                                        <div className="storeProfile">
-                                        </div>
-                                        <div className="storeInfoTime">
-                                            <div className="storeInfoList">
-                                                <div className="storeInfo">
-                                                    제이제이 헤어
-                                                </div>
-                                                <div className="storePartition">
-                                                    |
-                                                </div>
-                                                <div className="storeInfo">
-                                                    수원시 영통구 이의동
-                                                </div>
-                                                <div className="storePartition">
-                                                    |
-                                                </div>
-                                                <div className="storeInfo">
-                                                    헤어
-                                                </div>
-                                                <div className="storePartition">
-                                                    |
-                                                </div>
-                                                <div className="regularSign">
-                                                    ☆
-                                                </div>
-                                            </div>
-                                            <div className="reservInfo">
-                                                <div className="reservDate">
-                                                    24. 3. 4 월
-                                                </div>
-                                                <div className="reservTime">
-                                                    11:30
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="reservFrame">
-                                        <div className="EmpBox">
-                                            <div className="EmpNameTime">
-                                                <div className="EmpName">
-                                                    김진영 디자이너 -
-                                                </div>
-                                                <div className="EmpTime">
-                                                    11:30 타임
-                                                </div>
-                                            </div>
-                                            <div className="reservBoxLine">
-                                            </div>
-                                        </div>
-                                        <div className="reservContent">
-                                            <div className="reservMenuList">
-                                                [가게주인이 적은 타이틀], 소분류메뉴, 소분류메뉴
-                                            </div>
-                                        </div>
-                                        <div className="reservBoxLine">
-                                        </div>
-                                    </div>
-                                    <div className="cancelButton">
-                                        예약취소
-                                    </div>
-                                </div>
-                                <div className="currentReservBox">
-                                    <div className="storeInfoFrame">
-                                        <div className="storeProfile">
-                                        </div>
-                                        <div className="storeInfoTime">
-                                            <div className="storeInfoList">
-                                                <div className="storeInfo">
-                                                    레이슨 네일샵
-                                                </div>
-                                                <div className="storePartition">
-                                                    |
-                                                </div>
-                                                <div className="storeInfo">
-                                                    서울시 마포구 창전동
-                                                </div>
-                                                <div className="storePartition">
-                                                    |
-                                                </div>
-                                                <div className="storeInfo">
-                                                    네일
-                                                </div>
-                                                <div className="storePartition">
-                                                    |
-                                                </div>
-                                                <div className="regularSign">
-                                                    ☆
-                                                </div>
-                                            </div>
-                                            <div className="reservInfo">
-                                                <div className="reservDate">
-                                                    24. 2. 20 화
-                                                </div>
-                                                <div className="reservTime">
-                                                    9:30
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="reservFrame">
-                                        <div className="EmpBox">
-                                            <div className="EmpNameTime">
-                                                <div className="EmpName">
-                                                    노진영 디자이너 -
-                                                </div>
-                                                <div className="EmpTime">
-                                                    9:30 타임
-                                                </div>
-                                            </div>
-                                            <div className="reservBoxLine">
-                                            </div>
-                                        </div>
-                                        <div className="reservContent">
-                                            <div className="reservMenuList">
-                                                [가게주인이 적은 타이틀], 소분류메뉴, 소분류메뉴
-                                            </div>
-                                        </div>
-                                        <div className="reservBoxLine">
-                                        </div>
-                                    </div>
-                                    <div className="cancelButton">
-                                        예약취소
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+  return (
+    <div className="MyHomeCurrReserv">
+      <div className="MyHomeCurrReservBack">
+        <MyHomeLeftNavi />
+        <div className="containerAll">
+          <MyHomeTopNavi />
+          <div className="MyHomeCurrReservContainer">
+            <div className="Content">
+              <div className="sortingBox">
+                <>
+                  <Select
+                    className="sortBox"
+                    ref={selectInputRef}
+                    onChange={(e) => {
+                      if (e) {
+                        setSelectValue(e.value);
+                      } else {
+                        setSelectValue("");
+                      }
+                    }}
+                    options={options}
+                    placeholder="전체"
+                  />
+                </>
+              </div>
+              <div className="reservList">
+                <div className="currentReservBox">
+                  <div className="storeInfoFrame">
+                    <div className="storeProfile"></div>
+                    <div className="storeInfoTime">
+                      <div className="storeInfoList">
+                        <div className="storeInfo">글래드뷰티</div>
+                        <div className="storePartition">|</div>
+                        <div className="storeInfo">수원시 영통구 광교산로</div>
+                        <div className="storePartition">|</div>
+                        <div className="storeInfo">메이크업</div>
+                        <div className="storePartition">|</div>
+                        <div className="regularSign">☆</div>
+                      </div>
+                      <div className="reservInfo">
+                        <div className="reservDate">24. 6. 13 목</div>
+                        <div className="reservTime">12:30</div>
+                      </div>
                     </div>
+                  </div>
+                  <div className="reservFrame">
+                    <div className="EmpBox">
+                      <div className="EmpNameTime">
+                        <div className="EmpName">김예은 -</div>
+                        <div className="EmpTime">12:30 타임</div>
+                      </div>
+                      <div className="reservBoxLine"></div>
+                    </div>
+                    <div className="reservContent">
+                      <div className="reservMenuList">
+                        기본 메이크업, 웨딩 메이크업
+                      </div>
+                    </div>
+                    <div className="reservBoxLine"></div>
+                  </div>
+                  <div className="cancelButton">예약취소</div>
                 </div>
+                <div className="currentReservBox">
+                  <div className="storeInfoFrame">
+                    <div className="storeProfile"></div>
+                    <div className="storeInfoTime">
+                      <div className="storeInfoList">
+                        <div className="storeInfo">레이슨 네일샵</div>
+                        <div className="storePartition">|</div>
+                        <div className="storeInfo">서울시 마포구 창전동</div>
+                        <div className="storePartition">|</div>
+                        <div className="storeInfo">네일</div>
+                        <div className="storePartition">|</div>
+                        <div className="regularSign">☆</div>
+                      </div>
+                      <div className="reservInfo">
+                        <div className="reservDate">24. 2. 20 화</div>
+                        <div className="reservTime">9:30</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="reservFrame">
+                    <div className="EmpBox">
+                      <div className="EmpNameTime">
+                        <div className="EmpName">노진영 디자이너 -</div>
+                        <div className="EmpTime">9:30 타임</div>
+                      </div>
+                      <div className="reservBoxLine"></div>
+                    </div>
+                    <div className="reservContent">
+                      <div className="reservMenuList">
+                        [가게주인이 적은 타이틀], 소분류메뉴, 소분류메뉴
+                      </div>
+                    </div>
+                    <div className="reservBoxLine"></div>
+                  </div>
+                  <div className="cancelButton">예약취소</div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/pages/ReservationPage/Reservation_Complete.js
+++ src/pages/ReservationPage/Reservation_Complete.js
@@ -1,25 +1,39 @@
-import LoginAfterMainHeader from '../nav/LoginAfterMainHeader'
-import React from 'react';
-import '../MainPage/Main.css';
-import './Reservation.css';
-import StandByReservation_Owner from '../StandByReservation_Owner/StandByReservation';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import LoginAfterMainHeader from "../nav/LoginAfterMainHeader";
+import "../MainPage/Main.css";
+import "./Reservation.css";
+import StandByReservation_Owner from "../StandByReservation_Owner/StandByReservation";
 
 export default function Reservation_Complete() {
-    return (
-        <div className='main_padding'>
-            {/* 예약 완료 페이지 내용 */}
-            <LoginAfterMainHeader />
-            <div className='reservation_padding'>
-                예약 신청되었습니다.
-                <div className='buttons_box'>
-                    <button className='button_big'>메인 페이지</button>
-                    <button className='button_big'>예약 확인</button>
-                </div>
-                
-                <button className='button_small'>예약 소식 받기</button>
-            </div>
-            {/* <StandByReservation_Owner/> */}
+  const navigate = useNavigate();
+  const handleNavigateToMainPage = () => {
+    navigate("/");
+  };
+  const handleNavigateToMyHomeCurrReserv = () => {
+    navigate("/MyHomeCurrReserv");
+  };
 
+  return (
+    <div className="main_padding">
+      {/* 예약 완료 페이지 내용 */}
+      <LoginAfterMainHeader />
+      <div className="reservation_padding">
+        예약 신청되었습니다.
+        <div className="buttons_box">
+          <button className="button_big" onClick={handleNavigateToMainPage}>
+            메인 페이지
+          </button>
+          <button
+            className="button_big"
+            onClick={handleNavigateToMyHomeCurrReserv}
+          >
+            예약 확인
+          </button>
         </div>
-    );
+        <button className="button_small">예약 소식 받기</button>
+      </div>
+      {/* <StandByReservation_Owner/> */}
+    </div>
+  );
 }
